a8d08b3b792b9e4f65d1f262fe76b1e6f306d521
ArifAli-0 <warrobotrocx@gmail.com>
2025-02-15 03:51:09 +0530

    F+R 100% acuracy

--- PA1/workspace/F+R/receiver.c
+++ PA1/workspace/F+R/receiver.c
@@ -9,96 +9,47 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
+#define MAX_RECEIVED_BITS_SIZE 1000 * 1024 * 8
 size_t CHUNKS = 0;
-char *received_messages[MAX_CHUNKS] = {0};
-uint32_t *received_chunks[MAX_CHUNKS] = {0};
-
-char *reconstruct(uint32_t *received_bits, size_t length)
+size_t received_till_now = 0;
+int received_bits[MAX_RECEIVED_BITS_SIZE];
+#define OUTPUT_FILE "received.txt"
+void dump_bits(int *bits, size_t length, const char *out_file_name)
 {
-    size_t num_chars = length * 4;
-    char *output = (char *)malloc(num_chars + 1);
-    if (!output)
-    {
-        printf("unable to allocate buffer on heap");
-        return NULL;
-    }
-    num_chars = 0;
-    char c[4];
-    for (size_t i = 0; i < length; i++)
-    {
-        uint32_t value = received_bits[i];
-        if (value == 0x0000)
-            continue;
-        // ++num_chars;
-        // printf("%08x;", value);
-        c[0] = (value >> 24) & 0xFF;
-        c[1] = (value >> 16) & 0xFF;
-        c[2] = (value >> 8) & 0xFF;
-        c[3] = value & 0xFF;
-        for (int j = 0; j < 4; j++)
-        {
-            if (c[j] >= 32 && c[j] <= 126)
-            {
-                output[num_chars++] = c[j];
-                // printf(".%c", c[j]);
-            }
-        }
-    }
-    // printf("\n");
-
-    output[num_chars] = '\0';
-    // printf("recon %s", output);
-    return output;
-}
-
-void append_to_file(const char *filename, const char *message)
-{
-    FILE *file = fopen(filename, "a");
+    printf("Dumping bits to file %s\n", out_file_name);
+    FILE *file = fopen(out_file_name, "wb");
     if (!file)
     {
         perror("fopen");
         return;
     }
-    fprintf(file, "%s", message);
-    fclose(file);
-}
 
-void clear_file(const char *filename)
-{
-    FILE *file = fopen(filename, "w");
-    if (!file)
+    uint8_t byte = 0;
+    size_t bit_count = 0;
+
+    for (size_t i = 0; i < length; i++)
     {
-        perror("fopen");
-    }
-    fclose(file);
-}
-
-void cleanup()
-{
-    clear_file("received.txt");
-
-    for (int i = 0; i <= CHUNKS; i++)
-    {
-        if (received_messages[i])
+        if (bits[i])
         {
-            printf("\nchunk %d : %s", i, received_messages[i]);
-            append_to_file("received.txt", received_messages[i]);
-            free(received_messages[i]);
+            byte |= (1 << (7 - bit_count));
+        }
+        bit_count++;
+
+        if (bit_count == 8)
+        {
+            fwrite(&byte, sizeof(uint8_t), 1, file);
+            byte = 0;
+            bit_count = 0;
         }
     }
-
-    close_transmit();
-    exit(0);
-}
-
-void signal_handler(int signum)
-{
-    if (signum == SIGINT)
+    if (bit_count > 0)
     {
-        cleanup();
+        fwrite(&byte, sizeof(uint8_t), 1, file);
     }
+
+    fclose(file);
 }
+
 size_t ronlyreload(void *addr)
 {
     size_t time = rdtsc();
@@ -106,14 +57,14 @@ size_t ronlyreload(void *addr)
     size_t delta = rdtsc() - time;
     return delta;
 }
-bool rdetect_bit()
+int rdetect_bit()
 {
     size_t hit = 0, miss = 0, i = 0;
     size_t time = rdtsc();
     while (rdtsc() - time < SLOT_LENGTH)
     {
         size_t reload_time = ronlyreload((void *)(base + i));
-        i = (i + 64) % SHARED_ARRAY_SIZE; // bytes in shared text file in nearest multiple of 64        // max_rld = MAX(max_rld, reload_time);
+        i = (i + 64) % SHARED_ARRAY_SIZE; // bytes in shared text file in nearest multiple of 64
         if (reload_time > MIN_CACHE_MISS_CYCLES)
         {
             miss++;
@@ -124,31 +75,23 @@ bool rdetect_bit()
         }
         i++;
     }
-    // printf("R:: miss: %d hit: %d bit: %d\n", miss, hit, miss >= hit);
     return miss >= hit;
+}
+void sigint_handler(int signum)
+{
+    dump_bits(received_bits, received_till_now, OUTPUT_FILE);
+    exit(0);
 }
 
 int main()
 {
-    signal(SIGINT, signal_handler);
-
+    signal(SIGINT, sigint_handler);
     open_transmit(); // opening shared file
-
-    // calibrate();
-    // exit(0);
 
     CHUNKS = 0;
 
-    // for (int i = 0; i < MAX_CHUNKS; i++)
-    //     received_messages[i] = 0;
-    // // ull sync_ts;
-    // uint32_t *received_chunks[MAX_CHUNKS] = {0};
-    // CHUNKS = receive_data(received_chunks);
-
-    int size = 0;
     size_t maxtry = 1000, try = 0;
     int prev = 0, freq_10 = 0, curr;
-    char dump[500];
     start_sync();
     // wait for magic sequence
     while (freq_10 < 3 && ++try < maxtry)
@@ -162,28 +105,11 @@ int main()
     {
 
         int bit = rdetect_bit((__uint64_t)base);
-        dump[size++] = bit + '0';
-        if (size == 500)
+        received_bits[received_till_now++] = bit;
+        if (received_till_now == MAX_RECEIVED_BITS_SIZE)
         {
-            dump[size] = '\0';
-            printf("%s\n", dump);
+            dump_bits(received_bits, received_till_now, OUTPUT_FILE);
             break;
         }
     }
-    // printf("chunks %ld\n", CHUNKS);
-    // if (CHUNKS <= 0)
-    // {
-    //     printf("error in receiving");
-    //     exit(1);
-    // }
-    // for (int i = 0; i < MAX_CHUNKS; i++)
-    // {
-    //     if (!received_chunks[i])
-    //         continue;
-    //     received_messages[i] =
-    //         reconstruct(received_chunks[i], MESSAGE_CHUNK_LEN / 32);
-    //     free(received_chunks[i]);
-    // }
-
-    cleanup();
 }
--- PA1/workspace/F+R/sender.c
+++ PA1/workspace/F+R/sender.c
@@ -12,68 +12,6 @@
 #define MAX_LIMIT_BOOL 1000 * 1024 * 8 // 250 KB
 #define CHUNK_SIZE 512
 
-void send_string(const char *str)
-{
-    send_magic_seq();
-    while (*str)
-    {
-        char c = *str++;
-        for (int i = ASCII_BITS - 1; i >= 0; i--)
-        {
-            send_bit((c >> i) & 1);
-        }
-    }
-    send_magic_seq();
-}
-
-char *read_file(const char *filename)
-{
-    FILE *file = fopen(filename, "r");
-    if (!file)
-    {
-        perror("fopen");
-        return NULL;
-    }
-
-    // Seek to the end of the file to determine its size
-    fseek(file, 0, SEEK_END);
-    long length = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    // Allocate memory for the file content
-    char *content = (char *)malloc(length + 1);
-    if (!content)
-    {
-        perror("malloc");
-        fclose(file);
-        return NULL;
-    }
-
-    // Read the file content into the allocated memory
-    fread(content, 1, length, file);
-    content[length] = '\0'; // Null-terminate the string
-
-    fclose(file);
-    return content;
-}
-
-void stringToBinary(const char *str, char *binary)
-{
-    char temp[9];     // Each character needs 8 bits + 1 null terminator
-    binary[0] = '\0'; // Initialize the binary string
-    for (size_t i = 0; i < strlen(str); i++)
-    {
-        unsigned char ch = str[i];
-        // Convert the character to binary
-        for (int j = 7; j >= 0; j--)
-        {
-            temp[7 - j] = (ch & (1 << j)) ? '1' : '0';
-        }
-        temp[8] = '\0';       // Null-terminate the temporary string
-        strcat(binary, temp); // Append the binary representation to the result
-    }
-}
-
 size_t read_bool_file(const char *filename, bool *bits)
 {
     FILE *file = fopen(filename, "rb");
@@ -123,15 +61,10 @@ int main()
     open_transmit(); // opens the shared file
     bool bit_stream[MAX_LIMIT_BOOL];
     size_t bits_len = read_bool_file("msg.txt", bit_stream);
-    // for (int i = 0; i < bit_stream_len; i++)
+    // for (int i = 0; i < bits_len; i++)
     // {
     //     printf("%d", bit_stream[i]);
-    //     if (i % 8 == 7)
-    //     {
-    //         printf(" ");
-    //     }
     // }
-    // printf("\n");
     size_t bit_index = 0;
     start_sync();
     for (int i = 0; i < 3; i++)
@@ -147,57 +80,9 @@ int main()
         ssend_bit(bit_stream[bit_index]);
         bit_index++;
     }
-    // write_to_file("msg.txt", data);
-    // char binary[strlen(payload) * 8 + 1];
-    // stringToBinary(payload, binary);
-
-    // printf("Binary: %s\n", binary);
-    // int binary_len = strlen(binary);
-    // size_t index = 0;
-    // while (1)
-    // {
-    //     int bit = binary[index] - 48;
-    //     ssend_bit(bit);
-    //     if (++index == binary_len)
-    //         break;
-    // }
-
-    ////////////////////////// Constants //////////////////////////
-    // uint32_t *datas;
-    // size_t datas_len = string_2_uint32(payload, &datas);
-    // for (size_t i = 0; i < count; i++)
-    // {
-    //     /* code */
-    // }
-
-    // size_t bits_per_uint32 = sizeof(uint32_t) * 8;
-    // size_t uint32s_per_chunk = MESSAGE_CHUNK_LEN / bits_per_uint32;
-    // size_t num_chunks =
-    //     (datas_len + uint32s_per_chunk - 1) / uint32s_per_chunk; // Round up
-
-    /////////////////// Send the message in chunks ///////////////////
-    // for (size_t chunk = 0; chunk < num_chunks; chunk++)
-    // {
-    //     ////////// Index calculation for each chunk //////////
-    //     size_t start = chunk * uint32s_per_chunk;
-    //     size_t end = start + uint32s_per_chunk;
-    //     if (end > datas_len)
-    //     {
-    //         end = datas_len;
-    //     }
-    //     // Need to sync here
-    //     ////////// Looping over each 32 bit integer in chunk //////////
-    //     for (size_t i = start; i < end; i++)
-    //     {
-    //         //////// Looping over all bits in 32 bit integer ////////
-    //         for (int j = sizeof(uint32_t) * 8 - 1; j >= 0; j--)
-    //         {
-    //         }
-    //         printf("\n");
-    //     }
-    // }
 
     // free(payload);
     close_transmit();
+    printf("Transmission complete\n");
     return 0;
 }
