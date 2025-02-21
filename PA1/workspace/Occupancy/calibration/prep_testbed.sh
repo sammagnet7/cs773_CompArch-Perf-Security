c4f3bb0c7f425b3031c64844837c6da363a482c7
ArifAli-0 <warrobotrocx@gmail.com>
2025-02-21 23:26:12 +0530

    Add command to disable Hyper threading

--- PA1/workspace/Occupancy/calibration/prep_testbed.sh
+++ PA1/workspace/Occupancy/calibration/prep_testbed.sh
@@ -3,4 +3,7 @@ sudo systemctl stop gdm
 sudo systemctl stop containerd
 sudo systemctl stop snapd
 sudo systemctl stop snapd.socket
+# disable HW prefetchers
 sudo wrmsr -a 0x1a4 47
+# turn off HT
+echo off | sudo tee /sys/devices/system/cpu/smt/control
