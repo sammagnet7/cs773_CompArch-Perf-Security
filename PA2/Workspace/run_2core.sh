562d6bcf1c36e6716efe9c0bd91295bafa8d1862
ArifAli-0 <warrobotrocx@gmail.com>
2025-03-15 12:57:31 +0530

    @Arif Add miss count plots

--- PA2/Workspace/run_2core.sh
+++ PA2/Workspace/run_2core.sh
@@ -11,4 +11,5 @@ trace1=`sed -n ''$num1'p' workloads.txt | awk '{print $1}'`
 trace2=`sed -n ''$num2'p' workloads.txt | awk '{print $1}'`
 
 mkdir -p results #change the name of your folder as required 
-(${binary} -warmup_instructions 50000000 -simulation_instructions 50000000 -traces ${TRACE_DIR1}/${trace1} ${TRACE_DIR2}/${trace2}) > results/mix-${num1}-${num2}.txt & #change the name of the folder as required
+(${binary} -warmup_instructions 50000000 -simulation_instructions 50000000 -traces ${TRACE_DIR1}/${trace1} ${TRACE_DIR2}/${trace2}) > results/mix-${num1}-${num2}-test7.txt  #change the name of the folder as required
+wait
