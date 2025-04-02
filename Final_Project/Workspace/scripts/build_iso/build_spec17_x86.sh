64a075bb7293b5b976d776cab6a0c120c3854a72
Soumik Dutta <sammagnet7@gmail.com>
2025-04-02 10:41:47 +0530

    Add speccpu iso build script

---
+++ Final_Project/Workspace/scripts/build_iso/build_spec17_x86.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## Run this script from `Workspace` folder
+## Example: `cd Final_Project/Workspace`. Then run this script.
+
+BASE=$(pwd)
+
+## Place the `cpu2017-1.1.9.iso` file in `Final_Project/resources_gem5` folder
+cd ../resources_gem5/
+
+mkdir mountdir
+mkdir SPEC17_workloads
+
+sudo mount -o loop cpu2017-1.1.9.iso mountdir
+
+cd mountdir
+
+./install.sh -d ../SPEC17_workloads
+
+cd ..
+
+sudo umount mountdir
+
+rm -rf mountdir
+
+
+############################################
+
+# we have taken system configuration specific .config file from `SPEC17_workloads/config` folder and modified it
+# These .cfg file in `custom_configs` folder modified and commented as '#MODIFIED' 
+cp $BASE/scripts/build_iso/configs_cpu17/gcc-linux-x86_all.cfg SPEC17_workloads/config
+
+cd SPEC17_workloads
+
+source shrc
+
+runcpu --config=gcc-linux-x86_all.cfg --action=build all
+runcpu --config=gcc-linux-x86_all.cfg --action=run --fake all
+
+cd ..
+mv SPEC17_workloads $BASE/SPEC17_workloads
+
+cd $BASE
