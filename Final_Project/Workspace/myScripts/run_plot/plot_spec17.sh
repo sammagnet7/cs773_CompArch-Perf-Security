a761781174d1c27998231b36a706d85e65478b30
ab47 <ab.inpathtoadev@gmail.com>
2025-04-12 21:48:04 +0530

    l1,l2 latencies

--- Final_Project/Workspace/myScripts/run_plot/plot_spec17.sh
+++ Final_Project/Workspace/myScripts/run_plot/plot_spec17.sh
@@ -40,12 +40,16 @@ slowdown_ipc_file="$PLOTS_DIR/data/slowdown_ipc.stat"
 ipc_file="$PLOTS_DIR/data/ipc.stat"
 mpki_l1_file="$PLOTS_DIR/data/mpki_l1.stat"
 mpki_l2_file="$PLOTS_DIR/data/mpki_l2.stat"
+latency_l1_file="$PLOTS_DIR/data/latency_l1.stat"
+latency_l2_file="$PLOTS_DIR/data/latency_l2.stat"
 
 rm -f "$slowdown_file"
 rm -f "$slowdown_ipc_file"
 rm -f "$ipc_file"
 rm -f "$mpki_l1_file"
 rm -f "$mpki_l2_file"
+rm -f "$latency_l1_file"
+rm -f "$latency_l2_file"
 
 ATTR_IPC='system.cpu.ipc'
 if [[ "$OLD_STATS" == true ]]; then
@@ -60,6 +64,8 @@ else
   ATTR_L2MISS_TOTAL='system.cpu.dcache.overallMisses::total'
   ATTR_L1MISS_TOTAL='system.l2.overallMisses::total'
   ATTR_SIM_INSTS='simInsts'
+  AATR_L1MISS_LATENCY='system.cpu.dcache.overallAvgMissLatency::total'
+  AATR_L2MISS_LATENCY='system.l2.overallAvgMissLatency::total'
 fi
 
 
@@ -71,6 +77,8 @@ extract_data() {
   simSeconds=$(grep "$ATTR_SIM_SECONDS" "$stats_file" | awk '{print $2}')
   l1_misses=$(grep "$ATTR_L1MISS_TOTAL" "$stats_file" | awk '{print $2}')
   l2_misses=$(grep "$ATTR_L2MISS_TOTAL" "$stats_file" | awk '{print $2}')
+  l1_latency=$(grep "$AATR_L1MISS_LATENCY" "$stats_file" | awk '{print $2}')
+  l2_latency=$(grep "$AATR_L2MISS_LATENCY" "$stats_file" | awk '{print $2}')
   insts=$(grep "$ATTR_SIM_INSTS" "$stats_file" | awk '{print $2}')
 
   if [[ -z "$simSeconds" || -z "$ipc" || "$ipc" == "-1" || -z "$insts" || "$insts" -eq 0 ]]; then
@@ -81,7 +89,7 @@ extract_data() {
   mpki_l1=$(echo "scale=3; $l1_misses * 1000 / $insts" | bc -l)
   mpki_l2=$(echo "scale=3; $l2_misses * 1000 / $insts" | bc -l)
 
-  echo "$ipc $simSeconds $mpki_l1 $mpki_l2"
+  echo "$ipc $simSeconds $mpki_l1 $mpki_l2 $l1_latency $l2_latency"
 }
 
 # Lists for geometric mean calculation
@@ -93,6 +101,12 @@ mpki_l1_base=()
 mpki_l1_modded=()
 mpki_l2_base=()
 mpki_l2_modded=()
+l1_latency_base=()
+l1_latency_modded=()
+l2_latency_base=()
+l2_latency_modded=()
+l1_latency_ratios=()
+l2_latency_ratios=()
 
 for bench in "${BENCHMARK_ITEMS[@]}"; do
   benchmark_dir=$(find "$SPEC17_DIR" -maxdepth 1 -type d -name '*'"$bench"'_s' 2>/dev/null)/${SPEC_RUN_DIR_SUFFIX}
@@ -115,11 +129,15 @@ for bench in "${BENCHMARK_ITEMS[@]}"; do
     continue
   fi
 
-  read base_ipc base_simseconds base_mpki_l1 base_mpki_l2 <<<$(echo "$base_data" | tail -1)
-  read modded_ipc modded_simseconds modded_mpki_l1 modded_mpki_l2 <<<$(echo "$modded_data" | tail -1)
+  read base_ipc base_simseconds base_mpki_l1 base_mpki_l2  base_l1_latency base_l2_latency<<<$(echo "$base_data" | tail -1)
+  read modded_ipc modded_simseconds modded_mpki_l1 modded_mpki_l2 modded_l1_latency modded_l2_latency <<<$(echo "$modded_data" | tail -1)
 
   slowdown=$(echo "scale=4; $base_simseconds / $modded_simseconds" | bc -l)
   slowdown_ipc=$(echo "scale=4; $modded_ipc / $base_ipc" | bc -l)
+  l1_latency_ratio=$(echo "scale=3; $modded_l1_latency/$base_l1_latency" | bc -l)
+  l2_latency_ratio=$(echo "scale=3; $modded_l2_latency/$base_l2_latency" | bc -l)
+
+  echo "$bench $base_l1_latency $modded_l1_latency";
 
   slowdowns+=("$slowdown")
   slowdown_ipcs+=("$slowdown_ipc")
@@ -129,12 +147,20 @@ for bench in "${BENCHMARK_ITEMS[@]}"; do
   mpki_l1_modded+=("$modded_mpki_l1")
   mpki_l2_base+=("$base_mpki_l2")
   mpki_l2_modded+=("$modded_mpki_l2")
+  l1_latency_base+=("$base_l1_latency")
+  l1_latency_modded+=("$modded_l1_latency")
+  l2_latency_base+=("$modded_l2_latency")
+  l2_latency_modded+=("$modded_l2_latency")
+  l1_latency_ratios+=("$l1_latency_ratio")
+  l2_latency_ratios+=("$l2_latency_ratio")
 
   echo "$bench $slowdown" >>"$slowdown_file"
   echo "$bench $slowdown_ipc" >>"$slowdown_ipc_file"
   echo "$bench $base_ipc $modded_ipc" >>"$ipc_file"
   echo "$bench $base_mpki_l1 $modded_mpki_l1" >>"$mpki_l1_file"
   echo "$bench $base_mpki_l2 $modded_mpki_l2" >>"$mpki_l2_file"
+  echo "$bench $l1_latency_ratio" >>"$latency_l1_file"
+  echo "$bench $l2_latency_ratio" >>"$latency_l2_file"
 done
 
 geo_mean() {
@@ -155,6 +181,8 @@ echo "gmean $(geo_mean "${slowdown_ipcs[@]}")" >>"$slowdown_ipc_file"
 echo "gmean $(geo_mean "${ipcs_base[@]}") $(geo_mean "${ipcs_modded[@]}")" >>"$ipc_file"
 echo "gmean $(geo_mean "${mpki_l1_base[@]}") $(geo_mean "${mpki_l1_modded[@]}")" >>"$mpki_l1_file"
 echo "gmean $(geo_mean "${mpki_l2_base[@]}") $(geo_mean "${mpki_l2_modded[@]}")" >>"$mpki_l2_file"
+echo "gmean $(geo_mean "${l1_latency_ratios[@]}")" >>"$latency_l1_file"
+echo "gmean $(geo_mean "${l2_latency_ratios[@]}")" >>"$latency_l2_file"
 
 commands=(
   "gnuplot -e \"gem_flavour='${FLAVOUR}'\" -e \"input_file='data/slowdown.stat'\" -e \"output_png='plots/slowdown.png'\" -e  \"var_title='Speedup compared to non-secure baseline [${FLAVOUR}]'\" -e \"var_xlabel='Benchmarks'\" -e \"var_ylabel='Speedup'\" slowdown.gp"
@@ -162,6 +190,8 @@ commands=(
   "gnuplot -e \"gem_flavour='${FLAVOUR}'\" -e \"input_file='data/ipc.stat'\" -e \"output_png='plots/ipc.png'\" -e  \"var_title='IPC [${FLAVOUR}]'\" -e \"var_xlabel='Benchmarks'\" -e \"var_ylabel='IPC'\" double_bar.gp"
   "gnuplot -e \"gem_flavour='${FLAVOUR}'\" -e \"input_file='data/mpki_l1.stat'\" -e \"output_png='plots/mpki_l1.png'\" -e  \"var_title='MPKI L1 [${FLAVOUR}]'\" -e \"var_xlabel='Benchmarks'\" -e \"var_ylabel='MPKI (L1)'\" double_bar.gp"
   "gnuplot -e \"gem_flavour='${FLAVOUR}'\" -e \"input_file='data/mpki_l2.stat'\" -e \"output_png='plots/mpki_l2.png'\" -e  \"var_title='MPKI L2 [${FLAVOUR}]'\" -e \"var_xlabel='Benchmarks'\" -e \"var_ylabel='MPKI (L2)'\" double_bar.gp"
+  "gnuplot -e \"gem_flavour='${FLAVOUR}'\" -e \"input_file='data/latency_l1.stat'\" -e \"output_png='plots/l1_latency.png'\" -e  \"var_title='L1 latency [${FLAVOUR}]'\" -e \"var_xlabel='Benchmarks'\" -e \"var_ylabel='Latency (L1)'\" slowdown.gp"
+  "gnuplot -e \"gem_flavour='${FLAVOUR}'\" -e \"input_file='data/latency_l2.stat'\" -e \"output_png='plots/l2_latency.png'\" -e  \"var_title='L2 latency [${FLAVOUR}]'\" -e \"var_xlabel='Benchmarks'\" -e \"var_ylabel='Latency (L2)'\" slowdown.gp"
 )
 
 for cmd in "${commands[@]}"; do
